a17289a6ced5103264563c52b1ea85f28344dfa7
Langstone <kalipsomart@gmail.com>
2021-01-10 19:39:11 +0600

    change cli.js

--- src/cli.js
+++ src/cli.js
@@ -2,5 +2,3 @@ const readlineSync = require('readline-sync');
 // Wait for user's response.
 const userName = readlineSync.question('May I have your name? ');
 console.log(`Hello ${userName}!`);
-
-const xx  =123123
